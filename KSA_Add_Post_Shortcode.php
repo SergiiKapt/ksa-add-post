15fd0426e037edc4ccf4430d940719916c367845
Sergii <sergiikapt@gmail.com>
2021-02-13 07:18:23 +0200

    fix KSA_Add_Post_Shortcode

--- KSA_Add_Post_Shortcode.php
+++ KSA_Add_Post_Shortcode.php
@@ -1,29 +1,121 @@
 <?php
-/*
-Plugin Name: KSASK add post shortcode
-Plugin URI: https://ksask.net/
-Description: Adding a shortcode to create a post add form.
-Author: KSASK
-Author URI: https://ksask.net/
-Text Domain: ksa-add-post-shortcode
-Domain Path: /languages/
-Version: 1.0
-*/
 
-if ( ! defined( 'ABSPATH' ) ) {
-    die( 'Invalid request.' );
+class KSA_Add_Post_Shortcode
+{
+    public static function run()
+    {
+        add_shortcode('ksa_add_post_form', [__CLASS__, 'ksa_add_post_form_shortcode']);
+        add_action('wp_enqueue_scripts', [__CLASS__, 'ksa_addp_custom_scripts']);
+        add_action('wp_enqueue_scripts', [__CLASS__, 'ksa_addp_translate']);
+        add_action('wp_ajax_ksa_addp_submit', [__CLASS__, 'ksa_addp_submit_callback']);
+        add_action('wp_ajax_nopriv_ksa_addp_submit', [__CLASS__, 'ksa_addp_submit_callback']);
+    }
+
+    public static function ksa_add_post_form_shortcode()
+    {
+        if (is_user_logged_in()):
+
+            echo get_option('admin_email');
+            echo get_option('bloginfo');
+
+
+
+
+            ?>
+            <div id="ksa__addp" class="ksa__addp">
+                <div class="title__wrap">
+                    <p class="title"><?php echo __('Create post') ?></p>
+                </div>
+                <?php wp_nonce_field('ksa_addp_nonce'); ?>
+                <div class="field__wrap">
+                    <label for="#ksa__addp__title" class="label label__title"><?php echo __('Title:') ?></label>
+                    <input id="ksa__addp__title" class="input__title" type="text" name="title"
+                           placeholder="Your Name"
+                           autocomplete="on" value="hh]/rt ' f">
+                </div>
+                <div class="field__wrap">
+                    <label for="#ksa__addp__text" class="label label__text"><?php echo __('Text:') ?></label>
+                    <textarea id="ksa__addp__text" class="input__text" name="text" id="" cols="30"
+                              rows="3"></textarea>
+                </div>
+                <div class="btn__wrap">
+                    <input id="ksa__addp__submit" class="btn submit ksa__addp__submit" type="submit"
+                           name="lwCSFormSubmit" value="<?php echo __('Create post') ?>">
+                </div>
+            </div>
+        <?php
+        endif;
+    }
+
+    public
+    static function ksa_addp_custom_scripts()
+    {
+
+        wp_enqueue_script('ksa-addp-js', KSA_ADDP_P_URI . 'assets/js/ksa-addp.js', array('jquery'), null, true);
+        wp_enqueue_style('ksa-addp-style', KSA_ADDP_P_URI . 'assets/css/style.css');
+        wp_localize_script('ksa-addp-js', 'ksa_addp_data',
+            array(
+                'url' => admin_url('admin-ajax.php'),
+                'home_url' => home_url(),
+            )
+        );
+
+        wp_register_script('ksa-addp-translate-js', plugins_url('/js/translate.js'), array('wp-i18n'));
+    }
+
+
+    public
+    static function ksa_addp_translate()
+    {
+        $jsfile_url = plugins_url('/js/ksa-addp.js');
+
+        wp_enqueue_script('my-script', $jsfile_url);
+        wp_set_script_translations('my-script', 'myl10n', plugins_url('/js/translate.js'));
+    }
+
+    public
+    static function ksa_addp_submit_callback()
+    {
+        if (is_user_logged_in()) {
+
+            require_once($_SERVER['DOCUMENT_ROOT'] . '/wp-load.php');
+
+            if (!check_ajax_referer('ksa_addp_nonce')) {
+                wp_die();
+            }
+
+            if (post_exists($_POST['title'])) {
+                $response = ['status' => 'error', 'msg' => __('Post with this title already exists')];
+            } else {
+                self::createPost($_POST);
+                self::sendMAil($_POST['title'], $_POST['text']);
+                $response = ['status' => 'success', 'msg' => __('Post was created!')];
+            }
+
+            echo json_encode($response);
+        }
+
+        die;
+    }
+
+    public
+    static function createPost($data)
+    {
+        $post_data = array(
+            'post_title' => sanitize_text_field($data['title']),
+            'post_content' => $data['text'],
+            'post_status' => 'pending',
+            'post_author' => get_current_user_id(),
+        );
+
+    }
+    public static function sendMAil($title, $text)
+    {
+        $headers = "From: " . get_option('blogname') . "\r\n";
+        $msg = __('Title') . ": " . $title . ".\r\n";
+        $msg .= __('Text') . ":\r\n" . $text . ".\r\n";
+        $theme = __('create new post from KSASK add post shortcode');
+
+        wp_mail(get_option('admin_email'), $theme, $msg, $headers);
+    }
 }
-
-define('KSA_ADDP', 1);
-
-define('KSA_ADDP_P', __FILE__);
-
-define('KSA_ADDP_P_DIR', untrailingslashit(dirname(KSA_ADDP_P)));
-
-define( 'KSA_ADDP_P_URI', plugin_dir_url( __FILE__ ) );
-
-if ( ! class_exists( 'KSA_Add_Post_Shortcode' ) ) :
-    require_once KSA_ADDP_P_DIR . '/KSA_Add_Post_Shortcode.php';
-
-    KSA_Add_Post_Shortcode::run();
-endif;
